5b280c6e8b2dacc3e77aab2dee4788226e20af63
knapora <krzysztof.napora@op.pl>
2024-07-04 10:59:18 +0200

    added fonts, changed title

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 /**
  * Path to exercise folder
  */
-const exercisePath = "05_Dzien_3/03_Tworzenie_ukladu_strony/02_Wycieczki_w_kosmos";
+const exercisePath = "01_Car_Sharing";
 
 /**
  * Don't change those lines below
